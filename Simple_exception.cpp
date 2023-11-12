ab54e0fc5be102fe9ef7773e10c0d0639380bb33
Vitalya <vitalyabes.vb@gmail.com>
2023-11-12 10:35:07 +0300

    Second commit

--- Simple_exception.cpp
+++ Simple_exception.cpp
@@ -4,7 +4,7 @@
 
 int function(std::string &str, int& forbidden_length) {
     if (str.length() == forbidden_length) {
-        return -1;
+        throw std::invalid_argument("forbidden length");
     }
     return str.length();
 
@@ -16,17 +16,23 @@ int main()
     SetConsoleOutputCP(1251);
     int forb_length;
     std::string str;
+    int er = 0;
     std::cout << "Введите запретную длину: ";
     std::cin >> forb_length;
     std::cout << "Введите слово: ";
     std::cin >> str;
+    while(er != 1) { try {
 
-    while (function(str, forb_length) != -1) {
         std::cout << "Длина слова \"" << str << "\" равна " << function(str, forb_length) << "\n";
         std::cout << "Введите запретную длину: ";
         std::cin >> forb_length;
         std::cout << "Введите слово: ";
         std::cin >> str;
+
     }
-    std::cout << "Вы ввели слово запретной длины! До свидания";
+    catch (std::invalid_argument& e) {
+        std::cout << "Вы ввели слово запретной длины! До свидания";
+        er = 1;
+    }
+    }
 }
